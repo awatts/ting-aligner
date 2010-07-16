62c6e192d2bcae11b871afff543afaa6daa3858a
Andrew Watts <awatts@bcs.rochester.edu>
2010-07-16 14:12:35 -0400

    Used :retab in vim to get rid of hard tabs in align.pl

--- script/align.pl
+++ script/align.pl
@@ -56,48 +56,48 @@ my ($audio_fn, $text_fn, $manual_end) = @ARGV;
 # Press enter when the audio reaches the end of the currently displayed utterance.
 # Press Ctrl-D when done.
 sub find_manual_boundaries {
-	my $transcript = IO::File->new;
-	$transcript->open("transcript", "r") or croak "Can't open transcript: $!\n";
-	my $boundaries = IO::File->new;
-	$boundaries->open("boundaries", "r") or croak "Can't open boundaries: $!\n";
+    my $transcript = IO::File->new;
+    $transcript->open("transcript", "r") or croak "Can't open transcript: $!\n";
+    my $boundaries = IO::File->new;
+    $boundaries->open("boundaries", "r") or croak "Can't open boundaries: $!\n";
 
-	system("clear");
+    system("clear");
 
     print "====================================\n";
     print "Press ENTER when you hear a boundary\n";
     print "Then press CTRL + D\n";
     print "====================================\n";
 
-	unless (fork) {
-		`$ENV{APLAY} audio.wav 2>&1 >/dev/null`;
-	} else {
-		my $start = time;
+    unless (fork) {
+        `$ENV{APLAY} audio.wav 2>&1 >/dev/null`;
+    } else {
+        my $start = time;
 
-		my $line = <$transcript>;
-		print $line;
+        my $line = <$transcript>;
+        print $line;
 
-		while (<>) {
-			print $boundaries int((time - $start)*100) . "\n";
-			$line = <$transcript>;
-			print $line;
-		}
-	}
-	$transcript->close;
-	$boundaries->close;
+        while (<>) {
+            print $boundaries int((time - $start)*100) . "\n";
+            $line = <$transcript>;
+            print $line;
+        }
+    }
+    $transcript->close;
+    $boundaries->close;
 
-	return;
+    return;
 }
 
 # do initial per-segment processing of wav audio
 # based on process-audio.pl
 sub process_audio {
-	my $WAVE2FEAT = "$ENV{S3_BIN}/wave2feat";
-	my $S3EP = "$ENV{S3_BIN}/sphinx3_ep";
-	my $S3EP_MODELS = $ENV{S3EP_MODELS};
+    my $WAVE2FEAT = "$ENV{S3_BIN}/wave2feat";
+    my $S3EP = "$ENV{S3_BIN}/sphinx3_ep";
+    my $S3EP_MODELS = $ENV{S3EP_MODELS};
 
-	system("$WAVE2FEAT -i audio.wav -o mfc -mswav yes -seed 2");
-	system("$S3EP -input mfc -mean $S3EP_MODELS/means -mixw $S3EP_MODELS/mixture_weights -var $S3EP_MODELS/variances >ep");
-	return;
+    system("$WAVE2FEAT -i audio.wav -o mfc -mswav yes -seed 2");
+    system("$S3EP -input mfc -mean $S3EP_MODELS/means -mixw $S3EP_MODELS/mixture_weights -var $S3EP_MODELS/variances >ep");
+    return;
 }
 
 # subroutine to get the length of a wave file
@@ -118,15 +118,15 @@ sub get_wave_length {
 sub clean_transcript {
     my ($transcript_fn) = @_;
     my @new_transcript_text;
-	my $transcript_fp = IO::File->new;
-	$transcript_fp->open($transcript_fn, 'r') or croak "Cannot open $transcript_fn\n";
+    my $transcript_fp = IO::File->new;
+    $transcript_fp->open($transcript_fn, 'r') or croak "Cannot open $transcript_fn\n";
     while (<$transcript_fp>) {
-		chomp;
-		$_ =~ s/@\S+//igx;
-		$_ =~ s/\///gx;
-		push @new_transcript_text, $_;
+        chomp;
+        $_ =~ s/@\S+//igx;
+        $_ =~ s/\///gx;
+        push @new_transcript_text, $_;
     }
-	$transcript_fp->close;
+    $transcript_fp->close;
     return \@new_transcript_text;
 }
 
@@ -137,79 +137,79 @@ sub clean_transcript {
 sub write_to_file {
     my ($arr_ref, $output_fn) = @_;
     print @$arr_ref;
-	my $output_fp = IO::File->new;
-	$output_fp->open($output_fn, 'w') or croak "Cannot open $output_fn\n";
+    my $output_fp = IO::File->new;
+    $output_fp->open($output_fn, 'w') or croak "Cannot open $output_fn\n";
     foreach my $line (@$arr_ref) {
-		print $output_fp "$line\n";
+        print $output_fp "$line\n";
     }
     $output_fp->close;
-	return;
+    return;
 }
 
 # makes .transcript suitable for sphinx3_align using a ctl file
 # based on resegment-transcript.pl
 sub resegment_transcript {
 
-	my $ctl = IO::File->new;
-	my $transcript = IO::File->new;
-	my $insent = IO::File->new;
+    my $ctl = IO::File->new;
+    my $transcript = IO::File->new;
+    my $insent = IO::File->new;
 
-	$ctl->open('ctl', 'r') or croak "Can't open file ctl for reading: $!\n";
-	$transcript->open('transcript', 'r') or croak "Can't open file transcript for reading: $!\n";
-	$insent->open('insent', 'w') or croak "Can't open insent from writing: $!\n";
+    $ctl->open('ctl', 'r') or croak "Can't open file ctl for reading: $!\n";
+    $transcript->open('transcript', 'r') or croak "Can't open file transcript for reading: $!\n";
+    $insent->open('insent', 'w') or croak "Can't open insent from writing: $!\n";
 
-	my $uttNum = 0;
-	while (my $ctlLine = <$ctl>) {
-		chomp $ctlLine;
-		if ($ctlLine =~ /utt(?:\d+-)?(\d+)$/ix) {
-			my $segName = $&; # $& is the whole match
-			my $endUttNum = $1;
-			my @words = ();
-			while ($uttNum < $endUttNum) {
-				my $transcriptLine = <$transcript>;
-				$uttNum++;
-				chomp $transcriptLine;
-				$transcriptLine =~ s/^.*?: //x;
-				$transcriptLine =~ s/\[PARTIAL (\w+).*?\]/$1/gx;
-				$transcriptLine =~ s/\<.*?\>//gx;
-				$transcriptLine =~ s/\[.*?\]//gx;
-				$transcriptLine = uc($transcriptLine);
-				push @words, @{[ $transcriptLine =~ /[\w'-]+/gx ]};
-			}
-			print $insent join(' ', @words) . " ($segName)\n";
-		}
-	}
+    my $uttNum = 0;
+    while (my $ctlLine = <$ctl>) {
+        chomp $ctlLine;
+        if ($ctlLine =~ /utt(?:\d+-)?(\d+)$/ix) {
+            my $segName = $&; # $& is the whole match
+            my $endUttNum = $1;
+            my @words = ();
+            while ($uttNum < $endUttNum) {
+                my $transcriptLine = <$transcript>;
+                $uttNum++;
+                chomp $transcriptLine;
+                $transcriptLine =~ s/^.*?: //x;
+                $transcriptLine =~ s/\[PARTIAL (\w+).*?\]/$1/gx;
+                $transcriptLine =~ s/\<.*?\>//gx;
+                $transcriptLine =~ s/\[.*?\]//gx;
+                $transcriptLine = uc($transcriptLine);
+                push @words, @{[ $transcriptLine =~ /[\w'-]+/gx ]};
+            }
+            print $insent join(' ', @words) . " ($segName)\n";
+        }
+    }
 
-	$transcript->close;
-	$ctl->close;
-	return;
+    $transcript->close;
+    $ctl->close;
+    return;
 }
 
 # based very loosly on get-transcript-vocab.sh, which has gone from a one-liner
 # to a cleaner, safer, but much longer function
 sub get_transcript_vocab {
-	my $transcript = IO::File->new;
-	my $vocab = IO::File->new;
+    my $transcript = IO::File->new;
+    my $vocab = IO::File->new;
 
-	$transcript->open('transcript', 'r') or croak "Can't open file transcript for reading: $!\n";
-	$vocab->open('vocab.txt', 'w') or croak "Can't open file vocab.txt for writing: $!\n";
+    $transcript->open('transcript', 'r') or croak "Can't open file transcript for reading: $!\n";
+    $vocab->open('vocab.txt', 'w') or croak "Can't open file vocab.txt for writing: $!\n";
 
-	my @wordlist = ();
-	tie @wordlist, 'Array::Unique';
-	while (my $transline = <$transcript>) {
-		while ($transline =~ /([\w'-]+)/gx) {
-			push @wordlist, uc($1);
-		}
-	}
-	@wordlist = sort(@wordlist);
+    my @wordlist = ();
+    tie @wordlist, 'Array::Unique';
+    while (my $transline = <$transcript>) {
+        while ($transline =~ /([\w'-]+)/gx) {
+            push @wordlist, uc($1);
+        }
+    }
+    @wordlist = sort(@wordlist);
 
-	foreach my $word (@wordlist) {
-		print $vocab "$word\n";
-	}
+    foreach my $word (@wordlist) {
+        print $vocab "$word\n";
+    }
 
-	$vocab->close;
-	$transcript->close;
-	return;
+    $vocab->close;
+    $transcript->close;
+    return;
 }
 
 
@@ -219,82 +219,82 @@ sub get_transcript_vocab {
 #           (optional) save OOD words to file
 # Based on subdic by Lucian Galescu <galescu@cs.rochester.edu> <lgalescu@ihmc.us>
 sub subdic {
-	my %options = @_;
+    my %options = @_;
 
-	my $verbose = 0;
-	my $novar = 0;
-	my $ood_file = undef;
-	my $vocab_file = undef;
-	my $dict_file = undef;
-	my $subdic_file = undef;
-	my %vocab;
+    my $verbose = 0;
+    my $novar = 0;
+    my $ood_file = undef;
+    my $vocab_file = undef;
+    my $dict_file = undef;
+    my $subdic_file = undef;
+    my %vocab;
 
-	$novar = $options{'var'} if (defined $options{'var'});
-	$verbose = $options{'verbose'} if (defined $options{'verbose'});
+    $novar = $options{'var'} if (defined $options{'var'});
+    $verbose = $options{'verbose'} if (defined $options{'verbose'});
 
-	$ood_file = $options{'ood'};
-	$vocab_file = $options{'vocab'};
-	$dict_file = $options{'dictionary'};
-	$subdic_file = $options{'subdic'};
+    $ood_file = $options{'ood'};
+    $vocab_file = $options{'vocab'};
+    $dict_file = $options{'dictionary'};
+    $subdic_file = $options{'subdic'};
 
-	my $voc = IO::File->new;
-	$voc->open($vocab_file, 'r') or croak "Can't open vocab file: $!\n";
-	while (my $word = <$voc>) {
-		next if $word =~ /^\#\#/x; # skip header
-		chomp $word;
-		$word =~ s/^\s*(\S+)\s*$/$1/x;
-		# eliminate tags
-		next if $word =~ /^</x;
-		$vocab{$word} = 1;
-	}
-	$voc->close;
+    my $voc = IO::File->new;
+    $voc->open($vocab_file, 'r') or croak "Can't open vocab file: $!\n";
+    while (my $word = <$voc>) {
+        next if $word =~ /^\#\#/x; # skip header
+        chomp $word;
+        $word =~ s/^\s*(\S+)\s*$/$1/x;
+        # eliminate tags
+        next if $word =~ /^</x;
+        $vocab{$word} = 1;
+    }
+    $voc->close;
 
-	if ($verbose) {
-		warn "Read ", scalar(keys %vocab), " words.\n";
-	}
+    if ($verbose) {
+        warn "Read ", scalar(keys %vocab), " words.\n";
+    }
 
-	my $dic = IO::File->new;
-	my $subdic = IO::File->new;
-	$dic->open($dict_file, 'r') or croak "Can't open dictionary: $dict_file $!\n";
-	$subdic->open($subdic_file, 'w') or croak "Can't open subdic: $subdic_file $!\n";
-	while (<$dic>) {
+    my $dic = IO::File->new;
+    my $subdic = IO::File->new;
+    $dic->open($dict_file, 'r') or croak "Can't open dictionary: $dict_file $!\n";
+    $subdic->open($subdic_file, 'w') or croak "Can't open subdic: $subdic_file $!\n";
+    while (<$dic>) {
 
-		#
-		# This next statement was in the original, but made it skip every
-		# line. I'm commenting it out but leaving it in just in case. --alw
-		#
-		#next if /^##/x;
+        #
+        # This next statement was in the original, but made it skip every
+        # line. I'm commenting it out but leaving it in just in case. --alw
+        #
+        #next if /^##/x;
 
-		my $word;
-		if ($novar) {
-			if (m/^([^\s\(]+)/x) {
-				$word = $1;
-			}
-		} else {
-			if (m/^([^\s]+)\s/x) {
-				$word = $1;
-			}
-		}
+        my $word;
+        if ($novar) {
+            if (m/^([^\s\(]+)/x) {
+                $word = $1;
+            }
+        } else {
+            if (m/^([^\s]+)\s/x) {
+                $word = $1;
+            }
+        }
 
-		if (defined $vocab{$word}) {
-			print $subdic $_;
-			$vocab{$word} = 2;
-		}
-	}
-	$subdic->close;
-	$dic->close;
+        if (defined $vocab{$word}) {
+            print $subdic $_;
+            $vocab{$word} = 2;
+        }
+    }
+    $subdic->close;
+    $dic->close;
 
-	if (defined $ood_file) {
-		my $ood = IO::File->new;
-		$ood->open($ood_file, 'w') or croak "Can't open file $ood_file:  $!\n";
-		foreach my $word (sort keys %vocab) {
-			if ($vocab{$word} != 2) {
-				print $ood $word, "\n";
-			}
-		}
-		$ood->close;
-	}
-	return;
+    if (defined $ood_file) {
+        my $ood = IO::File->new;
+        $ood->open($ood_file, 'w') or croak "Can't open file $ood_file:  $!\n";
+        foreach my $word (sort keys %vocab) {
+            if ($vocab{$word} != 2) {
+                print $ood $word, "\n";
+            }
+        }
+        $ood->close;
+    }
+    return;
 }
 
 unless (defined $audio_fn && defined $text_fn) {
@@ -323,9 +323,9 @@ my @dirs = File::Spec->splitdir($cdirs);
 
 my $experiment;
 if ($#dirs >= 0) {
-	$experiment = "$dirs[-1]/$file/";
+    $experiment = "$dirs[-1]/$file/";
 } else {
-	$experiment = $file;
+    $experiment = $file;
 }
 
 # make a folder to store the files
@@ -341,24 +341,24 @@ chdir($experiment) ||
 get_transcript_vocab;
 
 subdic('var' => 1,
-	   'ood' => 'ood-vocab.txt',
-	   'vocab' =>'vocab.txt',
-	   'dictionary' => "$ENV{ALIGNER_DATA_HOME}/cmudict_0.6-lg_20060811.dic",
-	   'subdic' => 'vocab.dic'
+       'ood' => 'ood-vocab.txt',
+       'vocab' =>'vocab.txt',
+       'dictionary' => "$ENV{ALIGNER_DATA_HOME}/cmudict_0.6-lg_20060811.dic",
+       'subdic' => 'vocab.dic'
 );
 process_audio;
 
 if (defined $manual_end) {
-	# wait for user to define boundaries
-	find_manual_boundaries;
+    # wait for user to define boundaries
+    find_manual_boundaries;
 
-	# generate control file
-	my $ctl = Ctl->new;
-	$ctl->write_control_file;
+    # generate control file
+    my $ctl = Ctl->new;
+    $ctl->write_control_file;
 } else {
     # get the length of audio file
     # write control file
-	my $ctl = IO::File->new;
+    my $ctl = IO::File->new;
     $ctl->open('ctl', 'w') or croak;
     print $ctl "./\t0\t$length\tutt1\n";
     $ctl->close;
@@ -369,22 +369,22 @@ resegment_transcript;
 mkdir 'phseg';
 mkdir 'wdseg';
 system("$ENV{S3_BIN}/sphinx3_align \\
-	   -agc none \\
-	   -ctl ctl \\
-	   -cepext mfc \\
-	   -dict vocab.dic \\
-	   -fdict $ENV{ALIGNER_DATA_HOME}/filler.dic \\
-	   -mdef $ENV{S3_MODELS}/hub4opensrc.6000.mdef \\
-	   -mean $ENV{S3_MODELS}/means \\
-	   -mixw $ENV{S3_MODELS}/mixture_weights \\
-	   -tmat $ENV{S3_MODELS}/transition_matrices \\
-	   -var $ENV{S3_MODELS}/variances \\
-	   -insent insent \\
-	   -logfn s3alignlog \\
-	   -outsent outsent \\
-	   -phsegdir phseg \\
-	   -wdsegdir wdseg \\
-	   -beam 1e-80"
+       -agc none \\
+       -ctl ctl \\
+       -cepext mfc \\
+       -dict vocab.dic \\
+       -fdict $ENV{ALIGNER_DATA_HOME}/filler.dic \\
+       -mdef $ENV{S3_MODELS}/hub4opensrc.6000.mdef \\
+       -mean $ENV{S3_MODELS}/means \\
+       -mixw $ENV{S3_MODELS}/mixture_weights \\
+       -tmat $ENV{S3_MODELS}/transition_matrices \\
+       -var $ENV{S3_MODELS}/variances \\
+       -insent insent \\
+       -logfn s3alignlog \\
+       -outsent outsent \\
+       -phsegdir phseg \\
+       -wdsegdir wdseg \\
+       -beam 1e-80"
 );
 
 
